c5b23071249d6692a8bda4405d58e5d5ae0a12e8
hyper-ui <46814197+hyper-ui@users.noreply.github.com>
2019-01-20 03:23:33 +0000

    improve the example

--- demos/HelloWorld/index.js
+++ demos/HelloWorld/index.js
@@ -1,7 +1,7 @@
 // Define a component called `Greeting`
 const Greeting = HUI.define('Greeting', {
-    // Define the `render` method
-    render: function (props) {
+    // Define the renderer
+    render(props) {
         // Render a simple heading
         return HUI('h1', { style: { textAlign: 'center' } }, [
             'Hello,',
